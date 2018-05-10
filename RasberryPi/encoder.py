9420ba942e315f4f69374caa8bbf50e3c0862695
youliang <tan_you_liang@hotmail.com>
2018-05-10 16:06:48 +0800

    after exam

--- RasberryPi/encoder.py
+++ RasberryPi/encoder.py
@@ -4,9 +4,19 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 count = 0
 
+class Pulse:
+    def __init__(self):
+        self.state = 0
+        self.count = 0
+
+    def getNewCount(self, raw):
+        if raw == 1:
+            self.state = 1
+        if (self.state and raw == 0):
+            self.count = self.count + 1
+        return count
+
+pulse = Pulse()
 while (1):
-    pulse = GPIO.input(4)
-    if pulse != 0:
-        print "count: {}, result: {}, {}".format(count, pulse, type(pulse))
-    #print "NOW"
-    count = count +1
+    val =  pulse.getNewCount(GPIO.input(4))
+    print "encoder: ", val
